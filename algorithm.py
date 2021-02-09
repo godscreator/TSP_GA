d68e1f81aacf005dcc5925fb568501d7ad4dda54
godscreator <shubhamdragonsin@gmail.com>
2021-02-09 23:57:44 +0530

    made object oriented

--- algorithm.py
+++ algorithm.py
@@ -1,115 +1,150 @@
 import random
+import math
 
 
-def calculate_fitness(population, distance_matrix):
-    fitness = []
-    total_inverse_distances = []
-    for i in population:
-        d = 0
-        for j in range(len(i)):
-            d += distance_matrix[i[j]][i[(j + 1) % len(i)]]
-        total_inverse_distances.append(1 / d)
-    total_inverse_distance = sum(total_inverse_distances)
-    for i in range(len(population)):
-        fitness.append(total_inverse_distances[i] / total_inverse_distance)
-    return fitness
+class City:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def get_distance(A, B):
+        return math.sqrt(math.pow(A.x - B.x, 2) + math.pow(A.y - B.y, 2))
 
 
-def pick_one(population, fitness):
-    x = random.random()
-    s = 0
-    for i in range(len(population)):
-        s += fitness[i]
-        if x < s:
-            return population[i]
-    return population[-1]
+class TSP:
+    def __init__(self, population_size=10, cities=[]):
+        self.cities = cities
+        self.distance_matrix = []
+        self.calculate_distances()
 
+        self.population_size = population_size
+        self.population = []
+        self.fitness = []
+        self.initialize_population()
 
-def crossover(parent_a, parent_b):
-    a = random.randint(0, len(parent_a) - 1)
-    b = random.randint(a, len(parent_a))
-    if a == b:
-        return parent_b[:]
-    else:
-        child = [-1] * len(parent_a)
-        for i in range(a, b):
-            child[i] = parent_a[i]
-        i = 0
-        for j in parent_b:
-            if i == a:
-                i = b
-            if i >= len(parent_b):
-                break
-            if j not in child:
-                child[i] = j
-                i += 1
-        return child
+    def calculate_distances(self):
+        if len(self.cities) > 0:
+            self.distance_matrix = [[-1] * len(self.cities) for i in range(len(self.cities))]
+            # calculate distance matrix
+            for i in range(len(self.cities)):
+                for j in range(len(self.cities)):
+                    if self.distance_matrix[j][i] == -1:
+                        d = City.get_distance(self.cities[i], self.cities[j])
+                        self.distance_matrix[i][j] = d
+                    else:
+                        self.distance_matrix[i][j] = self.distance_matrix[j][i]
 
+    def initialize_population(self):
+        order_size = len(self.cities)
+        population_size = self.population_size
+        self.population = []
+        for i in range(population_size):
+            v = list(range(order_size))
+            random.shuffle(v)
+            self.population.append(v)
 
-def mutate(child, mutate_rate=0.01):
-    if random.random() < mutate_rate:
-        a = random.randrange(len(child))
-        b = random.randrange(len(child))
-        child[a], child[b] = child[b], child[a]
+    def calculate_fitness(self):
+        fitness = []
+        total_inverse_distances = []
+        for i in self.population:
+            d = 0
+            for j in range(len(i)):
+                d += self.distance_matrix[i[j]][i[(j + 1) % len(i)]]
+            total_inverse_distances.append(1 / d)
+        total_inverse_distance = sum(total_inverse_distances)
+        for i in range(len(self.population)):
+            fitness.append(total_inverse_distances[i] / total_inverse_distance)
+        self.fitness = fitness
 
+    def add_city(self, new_city):
+        self.cities.append(new_city)
+        self.distance_matrix.append([])
+        for i in range(len(self.cities) - 1):
+            self.distance_matrix[i].append(City.get_distance(self.cities[i], new_city))
+        for i in range(len(self.cities) - 1):
+            self.distance_matrix[-1].append(self.distance_matrix[i][-1])
+        self.distance_matrix[-1].append(0)
 
-def make_generation(population, fitness):
-    new_population = []
-    for i in range(len(population)):
-        parent_a = pick_one(population, fitness)
-        parent_b = pick_one(population, fitness)
-        child = crossover(parent_a, parent_b)
-        mutate(child)
-        new_population.append(child)
-    return new_population
+    def pick_one(self):
+        x = random.random()
+        s = 0
+        for i in range(len(self.population)):
+            s += self.fitness[i]
+            if x < s:
+                return self.population[i]
+        return self.population[-1]
 
+    @staticmethod
+    def crossover(parent_a, parent_b):
+        a = random.randint(0, len(parent_a) - 1)
+        b = random.randint(a, len(parent_a))
+        if a == b:
+            return parent_b[:]
+        else:
+            child = [-1] * len(parent_a)
+            for i in range(a, b):
+                child[i] = parent_a[i]
+            i = 0
+            for j in parent_b:
+                if i == a:
+                    i = b
+                if i >= len(parent_b):
+                    break
+                if j not in child:
+                    child[i] = j
+                    i += 1
+            return child
 
-best_ever = None
-best_fitness = 0
+    @staticmethod
+    def mutate(child, mutate_rate=0.01):
+        if random.random() < mutate_rate:
+            a = random.randrange(len(child))
+            b = random.randrange(len(child))
+            child[a], child[b] = child[b], child[a]
 
+    def make_generation(self):
+        new_population = []
+        for i in range(len(self.population)):
+            parent_a = self.pick_one()
+            parent_b = self.pick_one()
+            child = TSP.crossover(parent_a, parent_b)
+            TSP.mutate(child)
+            new_population.append(child)
+        self.population = new_population
 
-def initialize_population(population_size, order_size):
-    global best_ever
-    global best_fitness
-    best_ever = None
-    best_fitness = 0
-    population = []
-    for i in range(population_size):
-        v = list(range(order_size))
-        random.shuffle(v)
-        population.append(v)
-    return population
+    def derive_population(self):
+        best_ever = self.get_best_gene()
+        new_population = []
+        size = self.population_size
+        order_size = len(self.cities)
+        if best_ever:
+            for i in range(order_size - 1):
+                new_order = best_ever[:i] + [order_size - 1] + best_ever[i:]
+                new_population.append(new_order)
+                size -= 1
+        for i in range(size):
+            v = list(range(order_size))
+            random.shuffle(v)
+            new_population.append(v)
+        self.population = new_population
 
+    def get_best_index(self):
+        if self.fitness:
+            return self.fitness.index(max(self.fitness))
+        else:
+            return None
 
-def derive_population(population_size, order_size):
-    global best_ever
-    global best_fitness
-    new_population = []
-    size = population_size
-    if best_ever:
-        for i in range(order_size-1):
-            new_order = best_ever[:i]+[order_size-1]+best_ever[i:]
-            new_population.append(new_order)
-            size -= 1
+    def get_best_gene(self):
+        ind = self.get_best_index()
+        if ind:
+            return self.population[ind]
+        else:
+            return None
 
-    best_ever = None
-    best_fitness = 0
-
-    for i in range(size):
-        v = list(range(order_size))
-        random.shuffle(v)
-        new_population.append(v)
-    return new_population
-
-
-def run(population, distance_matrix):
-    fitness = calculate_fitness(population, distance_matrix)
-    if not fitness:
-        return population
-    x = max(fitness)
-    global best_fitness
-    global best_ever
-    if best_fitness < x:
-        best_fitness = x
-        best_ever = population[fitness.index(x)]
-    return make_generation(population, fitness)
+    def get_best_fitness(self):
+        ind = self.get_best_index()
+        if ind:
+            return self.fitness[ind]
+        else:
+            return None
--- main.py
+++ main.py
@@ -1,107 +1,125 @@
 import pygame
-import math
 import algorithm
 
 # initialize the pygame
 pygame.init()
 
 # create the screen
-screenX = 600
-screenY = 800
+screenX = 790
+screenY = 790
 screen = pygame.display.set_mode((screenX, screenY))
 
 # Title and Icon
 pygame.display.set_caption("Travelling Salesman")
 
-
 # cities
-class City:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+cities = []
 
-    def show(self, offset=(0, 0)):
-        pygame.draw.circle(screen, (255, 0, 0), (self.x+offset[0], self.y+offset[1]), 4)
-        pygame.draw.circle(screen, (255, 0, 0), (self.x+offset[0], self.y+offset[1]), 10, width=1)
+
+def draw_cities(cities, offset=(0, 0)):
+    for city in cities:
+        pygame.draw.circle(screen, (255, 0, 0), (city.x + offset[0], city.y + offset[1]), 4)
+        pygame.draw.circle(screen, (255, 0, 0), (city.x + offset[0], city.y + offset[1]), 10, width=1)
 
 
 # roads
-def make_road(x1, y1, x2, y2):
-    pygame.draw.line(screen, (0, 255, 0), (x1, y1), (x2, y2))
-
-
-def make_bad_road(x1, y1, x2, y2):
-    pygame.draw.line(screen, (255, 255, 255), (x1, y1), (x2, y2))
-
-
-# distance matrix
-distance_matrix = []
-
-
-def get_distance(city_A, city_B):
-    return math.sqrt(math.pow(city_A.x - city_B.x, 2) + math.pow(city_A.y - city_B.y, 2))
-
-
-def update_distance_matrix(cities, new_city):
-    distance_matrix.append([])
-    for i in range(len(cities) - 1):
-        distance_matrix[i].append(get_distance(cities[i], new_city))
-    for i in range(len(cities) - 1):
-        distance_matrix[-1].append(distance_matrix[i][-1])
-    distance_matrix[-1].append(0)
-
-
-cities = []
 population = []
+
+
+def draw_roads(order, offset=(0, 0), color=(255, 255, 255)):
+    if order and len(order) >= 2:
+        for i in range(len(order)):
+            a = cities[order[i]]
+            b = cities[order[(i + 1) % len(order)]]
+            pygame.draw.line(screen, color, (a.x + offset[0], a.y + offset[1]), (b.x + offset[0], b.y + offset[1]))
+
+
+clock = pygame.time.Clock()
+tsp = algorithm.TSP(population_size=100)
 count = 0
-x = True
+best_road = None
+
+rect1 = pygame.Rect(10, 10, 380, 380)
+best_panel = (10, 10, 380, 380)
+rect2 = pygame.Rect(10, 400, 380, 380)
+population_panel = (10, 400, 380, 380)
+rect3 = pygame.Rect(400, 10, 380, 380)
+update_panel = (400, 10, 380, 380)
+rect4 = pygame.Rect(400, 400, 380, 380)
+generation_panel = (400, 400, 380, 380)
+
+# text
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+
+def draw_text(s, pos):
+    text = font.render(s, True, (0, 255, 0))
+    textRect = text.get_rect()
+    textRect.center = pos
+    screen.blit(text, textRect)
+
+
+# draw graph
+def draw_graph(range_x, range_y, f, panel, div_x, div_y, scale_x, scale_y):
+    screen_origin = (panel[0] + 20, panel[1] + panel[3] - 20)
+    y_origin = screen_origin[1]
+    y_max = screen_origin[1] - panel[3] + 30
+    x_origin = screen_origin[0]
+    x_max = screen_origin[0] + panel[1] - 50
+    screen_scale_x = (x_max - x_origin) / (range_x[1] - range_x[0])
+    screen_scale_y = (y_max - y_origin) / (range_y[1] - range_y[0])
+    # y axis
+    pygame.draw.line(screen, (0, 0, 0), screen_origin, (x_origin, y_max))
+    # x axis
+    pygame.draw.line(screen, (0, 0, 0), screen_origin, (x_max, y_origin))
+    # label x axis
+    for i in range()
+
+
 # Game Loop
 running = True
 while running:
     # background
-    screen.fill((0, 0, 0))
-    if x:
-        pygame.draw.circle(screen, (255, 255, 255), (0, 0), 20)
-    x = not x
+    screen.fill((255, 0, 0))
+    pygame.draw.rect(screen, (255, 255, 255), rect1)
+    pygame.draw.rect(screen, (255, 255, 255), rect2)
+    pygame.draw.rect(screen, (255, 255, 255), rect3)
+    pygame.draw.rect(screen, (255, 255, 255), rect4)
     # handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            new_city = City(x, y)
-            cities.append(new_city)
-            update_distance_matrix(cities, new_city)
-            if len(cities) >= 2:
-                population = algorithm.derive_population(1000, len(cities))
-    if len(cities) >= 2 and count == len(population)-1:
-        population = algorithm.run(population, distance_matrix)
-    # all cities
-    for i in cities:
-        i.show()
-    # roads
-    road = algorithm.best_ever
-    if road and len(road) >= 2:
-        for i in range(len(road)):
-            a = road[i]
-            b = road[(i + 1) % len(road)]
-            make_road(cities[a].x, cities[a].y, cities[b].x, cities[b].y)
+            if best_panel[0] < x < best_panel[0] + best_panel[2] and best_panel[1] < y < best_panel[1] + best_panel[3]:
+                # add a new city
+                new_city = algorithm.City(x, y)
+                tsp.add_city(new_city)
+                tsp.derive_population()
+                cities = tsp.cities
+                population = tsp.population
+    if len(tsp.cities) >= 2 and count == len(population) - 1:
+        tsp.calculate_fitness()
+        tsp.make_generation()
+        tsp.calculate_fitness()
+        population = tsp.population
 
-    # all cities
-    for i in cities:
-        i.show(offset=(0, 300))
+    # display Best
+    draw_cities(cities)
+    road = tsp.get_best_gene()
+    fitness = tsp.get_best_fitness()
+    if road:
+        best_road = road
+    draw_roads(best_road, color=(0, 255, 0))
+
+    # display all population
+    draw_cities(cities, offset=(update_panel[0], update_panel[1]))
     if population:
         road = population[count]
         count = (count + 1) % len(population)
-    if road and len(road) >= 2:
-        for i in range(len(road)):
-            a = road[i]
-            b = road[(i + 1) % len(road)]
-            make_bad_road(cities[a].x, cities[a].y+300, cities[b].x, cities[b].y+300)
-    # # all roads
-    # for i in cities:
-    #     for j in cities:
-    #         if i!=j:
-    #             make_road(i.x, i.y, j.x, j.y)
+        draw_roads(road, color=(0, 0, 255), offset=(update_panel[0], update_panel[1]))
 
+    # display population graph
+    draw_graph(None,None,None,population_panel)
     pygame.display.update()
+    clock.tick(40)
